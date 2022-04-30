f4fad3e9770f8deab62ac8ace9a8aaf9b98390d3
Carlos Hollerveger <carlos.hollerveger@ntconsult.com.br>
2022-04-30 08:56:31 -0300

    added create task constraint

--- src/constraints/index.ts
+++ src/constraints/index.ts
@@ -2,3 +2,4 @@ export { createListConstraint } from './list/create';
 export { updateListConstraint } from './list/update';
 export { getListConstraint } from './list/get';
 export { deleteListConstraint } from './list/delete';
+export { createTaskConstraint } from './task/create';
