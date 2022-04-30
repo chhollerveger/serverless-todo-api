29d9ac1224b4403f3134b9cb68a806ea634c5cb7
Carlos Hollerveger <carlos.hollerveger@ntconsult.com.br>
2022-04-30 09:44:57 -0300

    add create task controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,3 +2,4 @@ export { CreateListController } from './list/create';
 export { UpdateListController } from './list/update';
 export { GetListController } from './list/get';
 export { DeleteListController } from './list/delete';
+export { CreateTaskController } from './task/create';
