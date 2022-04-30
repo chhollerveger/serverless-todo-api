e2f192b25ab561f35ff2bbe8d6c4e331f525555f
Carlos Hollerveger <carlos.hollerveger@ntconsult.com.br>
2022-04-30 09:44:35 -0300

    add create task controller

---
+++ src/controllers/task/create.ts
@@ -0,0 +1,25 @@
+
+import 'source-map-support/register';
+import { ResponseModel } from '@models';
+import { IController, ICreateTaskService, IHttpResponse, IValidator } from '@protocols';
+import { createTaskConstraint } from '@constraints';
+
+export class CreateTaskController implements IController {
+  constructor(
+    private validator: IValidator,
+    private createTaskService: ICreateTaskService,
+  ) { }
+
+  public async handle(body: string): Promise<IHttpResponse> {
+    try {
+      const request = JSON.parse(body);
+      this.validator.validateAgainstConstraints(request, createTaskConstraint());
+      const taskId = await this.createTaskService.create(request);
+      const response = new ResponseModel({ taskId }, 200, 'Task successfully added');
+      return response.generate();
+    } catch (error) {
+      const response = (error instanceof ResponseModel) ? error : new ResponseModel({}, 500, 'Task could not be added');
+      return response.generate();
+    }
+  }
+}
