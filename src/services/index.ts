da1f711f3b93216ccabede293a9b5fdb6c01287b
Carlos Hollerveger <carlos.hollerveger@ntconsult.com.br>
2022-04-30 09:41:56 -0300

    add create task service

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,3 +2,4 @@ export { CreateListService } from './list/create';
 export { UpdateListService } from './list/update';
 export { GetListService } from './list/get';
 export { DeleteListService } from './list/delete';
+export { CreateTaskService } from './task/create';
