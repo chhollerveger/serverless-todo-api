8491fdd3780ad53aa0c4951aaf617711251ba857
Carlos Hollerveger <carlos.hollerveger@ntconsult.com.br>
2022-05-01 10:41:03 -0300

    add update task factory

---
+++ src/factories/controllers/task/update.ts
@@ -0,0 +1,15 @@
+import { UpdateTaskController } from "@controllers";
+import { IController } from "@protocols";
+import { UpdateTaskService } from "@services";
+import { Validator } from "@utils"
+import { makeClientRepository } from "@factories";
+
+export const makeUpdateTaskController = (): IController => {
+  const validator = new Validator();
+  const clientRepository = makeClientRepository();
+  const service = new UpdateTaskService(clientRepository);
+  return new UpdateTaskController(
+    validator,
+    service
+  );
+}
--- src/factories/index.ts
+++ src/factories/index.ts
@@ -4,3 +4,4 @@ export { makeUpdateListController } from './controllers/list/update';
 export { makeDeleteListController } from './controllers/list/delete';
 export { makeGetListController } from './controllers/list/get';
 export { makeCreateTaskController } from './controllers/task/create';
+export { makeUpdateTaskController } from './controllers/task/update';
