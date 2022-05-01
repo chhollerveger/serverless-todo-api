1b2c5d84e84dec080f3307d4d40578715909f7f2
Carlos Hollerveger <carlos.hollerveger@ntconsult.com.br>
2022-05-01 10:41:48 -0300

    add update task handler

---
+++ src/handlers/task/update.ts
@@ -0,0 +1,7 @@
+import { APIGatewayEvent, APIGatewayProxyHandler, APIGatewayProxyResult, Context } from "aws-lambda";
+import { makeUpdateTaskController } from "@factories";
+
+export const handler: APIGatewayProxyHandler = (event: APIGatewayEvent, _context: Context): Promise<APIGatewayProxyResult> => {
+  const controller = makeUpdateTaskController();
+  return controller.handle(event.body);
+}
