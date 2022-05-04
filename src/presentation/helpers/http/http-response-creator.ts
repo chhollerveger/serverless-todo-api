886889dd67f79826de82171d8826c5217a27be67
Carlos Hollerveger <carlos.hollerveger@ntconsult.com.br>
2022-05-04 13:27:14 -0300

    add ajust error type

--- src/presentation/helpers/http/http-response-creator.ts
+++ src/presentation/helpers/http/http-response-creator.ts
@@ -1,6 +1,5 @@
 import { SuccessData } from "@presentation";
 import { HttpResponse, IGenericType, makeHttpResponseHeaders } from "@protocols"
-import { BadRequestError } from "../response/bad-request-error";
 import { ServerError } from "../response/server-error";
 import { StatusCode } from "./status";
 
@@ -12,16 +11,16 @@ export class HttpResponseCreator {
     body: new SuccessData(message, data)
   });
 
-  public static badRequest = (error: BadRequestError): HttpResponse => ({
+  public static badRequest = (error: Error): HttpResponse => ({
     statusCode: StatusCode.BadRequest,
     headers: makeHttpResponseHeaders(),
     body: error
   });
 
-  public static serverError = (error: ServerError): HttpResponse => ({
+  public static serverError = (error: Error): HttpResponse => ({
     statusCode: StatusCode.ServerError,
     headers: makeHttpResponseHeaders(),
-    body: (error instanceof ServerError) ? error : new ServerError('Internal server error', null)
+    body: (error instanceof ServerError) ? error : new ServerError('Internal server error', error.stack)
   });
 }
 
