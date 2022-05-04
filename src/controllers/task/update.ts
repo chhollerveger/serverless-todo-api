2ff18fa28f3431c21d4d0e80a131365a09e030e2
Carlos Hollerveger <carlos.hollerveger@ntconsult.com.br>
2022-05-04 14:37:19 -0300

    added error handling in task update controller

--- src/controllers/task/update.ts
+++ src/controllers/task/update.ts
@@ -3,7 +3,7 @@ import 'source-map-support/register';
 import { IController, IUpdateTaskService, IValidator, TaskDto } from '@protocols';
 import { updateTaskConstraint } from '@constraints';
 import { converterToType } from '@utils';
-import { HttpResponse, HttpResponseCreator } from '@presentation';
+import { BadRequestError, HttpResponse, HttpResponseCreator } from '@presentation';
 
 export class UpdateTaskController implements IController {
   constructor(
@@ -18,6 +18,11 @@ export class UpdateTaskController implements IController {
         return HttpResponseCreator.badRequest(error);
       }
       const request = converterToType(data, TaskDto);
+      const isCompletedPresent = typeof request.completed !== 'undefined';
+      if (!request.description && !isCompletedPresent) {
+        const present = ['description', 'completed'];
+        return HttpResponseCreator.badRequest(new BadRequestError('Invalid request: at least one of them must be present.', { present }));
+      }
       const results = await this.updateTaskService.update(request);
       return HttpResponseCreator.success('Task successfully updated', { ...results.Attributes })
     } catch (error) {
