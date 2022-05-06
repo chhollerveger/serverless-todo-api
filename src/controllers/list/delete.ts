d05d25de1fb0d28fa57aa63e53d6ef15eb02ba23
Carlos Hollerveger <carlos.hollerveger@ntconsult.com.br>
2022-05-05 21:24:41 -0300

    added ajustments request params

--- src/controllers/list/delete.ts
+++ src/controllers/list/delete.ts
@@ -1,7 +1,6 @@
 import 'source-map-support/register';
 import { IController, IDeleteListService, IValidator, ListRequestDto } from '@protocols';
 import { getListConstraint } from '@constraints';
-import { converterToType } from '@utils';
 import { HttpResponse, HttpResponseCreator } from '@presentation';
 
 export class DeleteListController implements IController {
@@ -10,14 +9,13 @@ export class DeleteListController implements IController {
     private deleteListService: IDeleteListService,
   ) { }
 
-  public async handle(params: string): Promise<HttpResponse> {
+  public async handle(params: ListRequestDto): Promise<HttpResponse> {
     try {
-      const request = converterToType(params, ListRequestDto);
-      const error = this.validator.validateAgainstConstraints(request, getListConstraint());
+      const error = this.validator.validateAgainstConstraints(params, getListConstraint());
       if (error) {
         return HttpResponseCreator.badRequest(error);
       }
-      await this.deleteListService.delete(request);
+      await this.deleteListService.delete(params);
       return HttpResponseCreator.success('To-do list successfully deleted');
     } catch (error) {
       return HttpResponseCreator.serverError(error);
