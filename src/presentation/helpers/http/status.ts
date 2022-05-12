2dc7ada6ef099275694b85f4062e40508ba3d92f
Carlos Hollerveger <carlos.hollerveger@ntconsult.com.br>
2022-05-12 19:15:01 -0300

    added status code created

--- src/presentation/helpers/http/status.ts
+++ src/presentation/helpers/http/status.ts
@@ -1,5 +1,6 @@
 export enum StatusCode {
   Success = 200,
+  Created = 201,
   BadRequest = 400,
   NotFound = 404,
   ServerError = 500,
@@ -7,6 +8,7 @@ export enum StatusCode {
 
 export enum StatusName {
   Success = 'Success',
+  Created = 'Created',
   BadRequest = 'Bad Request',
   NotFound = 'Not Found',
   ServerError = 'Server Error',
