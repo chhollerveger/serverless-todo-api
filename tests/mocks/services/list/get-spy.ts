ca358a8fe645230ead85811d11e09ebc5d48ff61
Carlos Hollerveger <carlos.hollerveger@ntconsult.com.br>
2022-05-26 13:21:18 -0300

    added mock list data on get list service spy

--- tests/mocks/services/list/get-spy.ts
+++ tests/mocks/services/list/get-spy.ts
@@ -1,29 +1,8 @@
 import { IGetListService, IListResponseDto, ListRequestDto } from "@protocols";
+import { mockListData } from "../../../mocks/data/list-data";
 
 export class GetListServiceSpy implements IGetListService {
-  data: IListResponseDto = {
-    id: '61470607-e7c4-486a-9334-fc2af1f4c1e5',
-    name: 'my todo list',
-    createdAt: 1653575340652,
-    updatedAt: 1653575340652,
-    taskCount: 2,
-    tasks: [
-      {
-        id: '94611e24-ba70-4a01-8e00-5f1b4e7c4552',
-        description: 'my task todo list 1',
-        completed: false,
-        createdAt: 1653575389761,
-        updatedAt: 1653575389761
-      },
-      {
-        id: '0f87c7d0-82c6-4d98-9b3a-ea1bca666afa',
-        description: 'my task todo list 2',
-        completed: true,
-        createdAt: 1653575394579,
-        updatedAt: 1653575578311
-      }
-    ]
-  };
+  data: IListResponseDto = mockListData();
 
   async get(params: ListRequestDto): Promise<IListResponseDto> {
     if (params.listId !== this.data.id) {
