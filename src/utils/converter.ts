cf14f2a790d0c1b263086db9d6bf702e902ec563
Carlos Hollerveger <carlos.hollerveger@ntconsult.com.br>
2022-05-26 13:45:49 -0300

    added catch error converter type

--- src/utils/converter.ts
+++ src/utils/converter.ts
@@ -1,7 +1,13 @@
+import { BadRequestError } from "@presentation";
+
 export function converterToType<T>(body: any, type: new () => T, params: any = undefined): T {
-  const taregt = new type();
-  const parsedData = JSON.parse(body);
-  Object.assign(taregt, parsedData, params);
-  Object.keys(taregt).forEach((key) => taregt[key] === undefined && delete taregt[key]);
-  return taregt;
+  try {
+    const taregt = new type();
+    const parsedData = JSON.parse(body);
+    Object.assign(taregt, parsedData, params);
+    Object.keys(taregt).forEach((key) => taregt[key] === undefined && delete taregt[key]);
+    return taregt;
+  } catch (error) {
+    throw new BadRequestError(error.message);
+  }
 }
